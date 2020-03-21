7764c1191cdcfb7d6e04c50aa9cb01d937cc00a4
Antoine Castel <castel.antoine95@gmail.com>
2020-03-21 01:49:16 +0100

    correction

--- reporter.py
+++ reporter.py
@@ -20,49 +20,37 @@ def emoji_face(new_cases_number):
             return emoji_sick_faces[key]
     return '🥺'
 
-
-file1 = open("registered_date.txt", "r")
-registered__date = file1.read()
-file1.close()
-
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
       "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 s = requests.get(url).content
 confirmed = pd.read_csv(io.StringIO(s.decode('utf-8')))
 last_update_day = datetime.strptime(confirmed.columns[-1], "%m/%d/%y").date()
+total_countries = confirmed.groupby("Country/Region").sum()
+two_last_days = total_countries[total_countries.columns[-2:]]
+new_cases = two_last_days[two_last_days.columns[-1]] - two_last_days[two_last_days.columns[-2]]
+top_five_countries = new_cases.sort_values(ascending=False).head(5)
 
-if last_update_day != datetime.strptime(registered__date, "%Y-%m-%d").date():
+tweet = "{} // COVID-19 NEW CASES TOP 5 COUNTRIES REPORT\n\n".format(last_update_day)
+i = 1
+for items in top_five_countries.iteritems():
+    country = items[0]
+    new_cases_number = items[1]
+    total = total_countries[total_countries.columns[-1]][country]
 
-    total_countries = confirmed.groupby("Country/Region").sum()
-    two_last_days = total_countries[total_countries.columns[-2:]]
-    new_cases = two_last_days[two_last_days.columns[-1]] - two_last_days[two_last_days.columns[-2]]
-    top_five_countries = new_cases.sort_values(ascending=False).head(5)
+    tweet += "{}. {} : +{} {}  +{}% (total: {})\n".format(i,
+                                                          flag.flag(country_isos[country]),
+                                                          new_cases_number,
+                                                          emoji_face(new_cases_number),
+                                                          round(
+                                                              100 * (new_cases_number / (total - new_cases_number)),
+                                                              1),
+                                                          total
+                                                          )
+    i = i + 1
 
-    tweet = "{} // COVID-19 NEW CASES TOP 5 COUNTRIES REPORT\n\n".format(last_update_day)
-    i = 1
-    for items in top_five_countries.iteritems():
-        country = items[0]
-        new_cases_number = items[1]
-        total = total_countries[total_countries.columns[-1]][country]
+secrets = get_secrets()
+auth = tweepy.OAuthHandler(secrets[0], secrets[1])
+auth.set_access_token(secrets[2], secrets[3])
 
-        tweet += "{}. {} : +{} {}  +{}% (total: {})\n".format(i,
-                                                              flag.flag(country_isos[country]),
-                                                              new_cases_number,
-                                                              emoji_face(new_cases_number),
-                                                              round(
-                                                                  100 * (new_cases_number / (total - new_cases_number)),
-                                                                  1),
-                                                              total
-                                                              )
-        i = i + 1
-
-    secrets = get_secrets()
-    auth = tweepy.OAuthHandler(secrets[0], secrets[1])
-    auth.set_access_token(secrets[2], secrets[3])
-
-    api = tweepy.API(auth)
-    api.update_status(tweet)
-
-    file1 = open("registered__date.txt", "w")
-    file1.write(str(last_update_day))
-    file1.close()
+api = tweepy.API(auth)
+api.update_status(tweet)
